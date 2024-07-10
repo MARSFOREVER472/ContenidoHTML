121f0c9cb6ce0abef8f477207753b0ece5a81f52
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-07-10 18:17:37 -0400

    Textos cambiados en JavaScript!

--- ArchivosEjecutables/script.js
+++ ArchivosEjecutables/script.js
@@ -1,1 +1,2 @@
-document.getElementById("mydiv").innerHTML = "Changing Text Here!";
+// document.getElementById("mydiv").innerHTML = "Changing Text Here!";
+document.querySelector("#mydiv").innerHTML = "Text Changed Using QuerySelector!"
